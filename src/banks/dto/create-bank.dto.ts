51767c32fd2a5f3d55738e1784bdba265d8722da
sidnik0 <sidnik199918@gmail.com>
2021-08-04 17:14:58 +0300

    Add Banks Module

---
+++ src/banks/dto/create-bank.dto.ts
@@ -0,0 +1,5 @@
+export class CreateBankDto {
+  readonly id: string;
+  readonly name: string;
+  readonly commission: number;
+}
---
+++ src/banks/interfaces/bank.interface.ts
@@ -0,0 +1,5 @@
+export interface BankInterface {
+  id: string;
+  name: string;
+  commission: number;
+}
